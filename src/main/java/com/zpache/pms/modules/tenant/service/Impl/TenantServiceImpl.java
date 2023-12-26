35205632a71c5d3c77520afe9e916b4d2c94ce9a
zpache <ryanwang_best@126.com>
2023-12-26 17:31:15 +0800

    add tenant system

---
+++ src/main/java/com/zpache/pms/modules/tenant/service/Impl/TenantServiceImpl.java
@@ -0,0 +1,11 @@
+package com.zpache.pms.modules.tenant.service.Impl;
+
+import com.zpache.pms.modules.tenant.service.TenantService;
+
+/**
+ * @desc:
+ * @author: wangbing
+ * @createTime: 2023/12/26 17:30
+ */
+public class TenantServiceImpl implements TenantService {
+}
---
+++ src/main/java/com/zpache/pms/modules/tenant/service/TenantService.java
@@ -0,0 +1,9 @@
+package com.zpache.pms.modules.tenant.service;
+
+/**
+ * @desc:
+ * @author: wangbing
+ * @createTime: 2023/12/26 17:30
+ */
+public interface TenantService {
+}
