be306ed14277e9d46fcd4c6de5b9b14f0d59f048
zpache <ryanwang_best@126.com>
2023-12-26 17:31:15 +0800

    init project

---
+++ src/test/java/com/zpache/pms/PmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpache.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
