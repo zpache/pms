80ee35b906c6c39f3554b8a0e5ed3e48cd5b7953
zpache <ryanwang_best@126.com>
2023-12-29 17:42:50 +0800

    完善用户管理

--- src/main/java/com/zpache/pms/modules/user/controller/UserController.java
+++ src/main/java/com/zpache/pms/modules/user/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.zpache.pms.modules.user.controller;
 
+import com.zpache.pms.common.base.PmsResult;
+import com.zpache.pms.common.utils.ResultUtils;
+import com.zpache.pms.modules.user.form.UserForm;
 import com.zpache.pms.modules.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/list")
+    public PmsResult list(@RequestBody UserForm userForm) {
+        return ResultUtils.success(userService.list(userForm));
+    }
+
+    @PostMapping("/detail")
+    public PmsResult detail(@RequestBody UserForm userForm) {
+        return ResultUtils.success(userService.getById(userForm.getId()));
+    }
+
+    @PostMapping("/save")
+    public PmsResult save(@RequestBody UserForm userForm) {
+        return ResultUtils.success(userService.save(userForm));
+    }
+
+    @PostMapping("/delete")
+    public PmsResult delete(@RequestBody UserForm userForm) {
+        userService.delete(userForm.getId());
+        return ResultUtils.success();
+    }
 }
