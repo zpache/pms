9236565a169c933eb2d57693d95f4fa8f4526a96
zpache <ryanwang_best@126.com>
2024-01-02 09:37:23 +0800

    完善角色维护相关接口

--- src/main/java/com/zpache/pms/modules/role/controller/RoleController.java
+++ src/main/java/com/zpache/pms/modules/role/controller/RoleController.java
@@ -1,7 +1,12 @@
 package com.zpache.pms.modules.role.controller;
 
+import com.zpache.pms.common.base.PmsResult;
+import com.zpache.pms.common.utils.ResultUtils;
+import com.zpache.pms.modules.role.form.RoleForm;
 import com.zpache.pms.modules.role.service.RoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RoleController {
     private final RoleService roleService;
+
+    @PostMapping("/list")
+    public PmsResult list(@RequestBody RoleForm roleForm) {
+        return ResultUtils.success(roleService.list(roleForm));
+    }
+
+
+    @PostMapping("/save")
+    public PmsResult save(@RequestBody RoleForm roleForm) {
+        return ResultUtils.success(roleService.save(roleForm));
+    }
+
+    @PostMapping("/delete")
+    public PmsResult delete(@RequestBody RoleForm roleForm) {
+        roleService.delete(roleForm.getId());
+        return ResultUtils.success();
+    }
 
 }
